a76456207d6b9aabc028ba9d1762582d19c2c196
patrick.reybaud <patrick.reybaud@devisubox.com>
2019-09-07 13:09:20 +0200

    update gitignore 	nouveau fichier : Release/objects.mk

---
+++ Release/Drivers/CMSIS/DSP/Source/BasicMathFunctions/subdir.mk
@@ -0,0 +1,73 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_abs_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_abs_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_add_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_add_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_dot_prod_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_dot_prod_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_mult_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_mult_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_negate_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_negate_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_offset_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_offset_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_scale_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_scale_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_shift_q31.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_sub_f32.c \
+../Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_sub_q31.c 
+
+OBJS += \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_abs_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_abs_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_add_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_add_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_dot_prod_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_dot_prod_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_mult_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_mult_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_negate_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_negate_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_offset_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_offset_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_scale_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_scale_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_shift_q31.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_sub_f32.o \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_sub_q31.o 
+
+C_DEPS += \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_abs_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_abs_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_add_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_add_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_dot_prod_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_dot_prod_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_mult_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_mult_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_negate_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_negate_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_offset_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_offset_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_scale_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_scale_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_shift_q31.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_sub_f32.d \
+./Drivers/CMSIS/DSP/Source/BasicMathFunctions/arm_sub_q31.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/DSP/Source/BasicMathFunctions/%.o: ../Drivers/CMSIS/DSP/Source/BasicMathFunctions/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m7 -mthumb -mfloat-abi=hard -mfpu=fpv5-d16 '-D__weak=__attribute__((weak))' -D__FPU_PRESENT -DARM_MATH_CM7 '-D__packed="__attribute__((__packed__))"' -DUSE_HAL_DRIVER -DSTM32H743xx -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Core/Inc" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Drivers/CMSIS/DSP/Include" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Middleware/Inc" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Peripheral/Inc" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Drivers/STM32H7xx_HAL_Driver/Inc" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Drivers/STM32H7xx_HAL_Driver/Inc/Legacy" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Drivers/CMSIS/Device/ST/STM32H7xx/Include" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Drivers/CMSIS/Include" -I"/mnt/data/Documents/Workspace_AC6/CISYNTH/Drivers/CMSIS/DSP/Include"  -O3 -Ofast -Wall -fmessage-length=0 -ffunction-sections -fdata-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
