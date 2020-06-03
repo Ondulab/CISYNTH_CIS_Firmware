d53e3082bd24d9e21e18800d9eb244735d0f1244
patrick.reybaud <patrick.reybaud@devisubox.com>
2020-06-04 01:35:59 +0200

    Audio DAC example work!!!

--- Debug/sources.mk
+++ Debug/sources.mk
@@ -21,6 +21,7 @@ Application/Src \
 Core/Src \
 Drivers/BSP/Components/ft5336 \
 Drivers/BSP/Components/mt48lc4m32b2 \
+Drivers/BSP/Components/wm8994 \
 Drivers/BSP/STM32H750B-Discovery \
 Drivers/STM32H7xx_HAL_Driver/Src \
 Peripheral/Src \
