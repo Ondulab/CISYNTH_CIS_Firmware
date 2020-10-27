fb55e3c20504b628ae23d368ccef98199cd9826f
patrick.reybaud <patrick.reybaud@devisubox.com>
2020-10-27 13:08:41 +0100

    First touchGFX generation build

--- TouchGFX/App/app_touchgfx.h
+++ TouchGFX/App/app_touchgfx.h
@@ -15,15 +15,7 @@
   ******************************************************************************
   */
 
-#ifdef __cplusplus
- extern "C" {
-#endif
-
 void MX_TouchGFX_Init(void);
 void MX_TouchGFX_Process(void);
-
-#ifdef __cplusplus
-}
-#endif
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
