2f4600dd872ebcb682df6722374bddb136aa9a61
zhonx <patrick.reybaud@devisubox.com>
2021-05-15 22:02:49 +0200

    Update UDP communication, add SSS3 header

--- CM7/Application/Inc/config.h
+++ CM7/Application/Inc/config.h
@@ -8,28 +8,28 @@
 /**************************************************************************************/
 /********************              STM32 definitions               ********************/
 /**************************************************************************************/
-#define FLASH_BASE_ADDR      (uint32_t)(FLASH_BASE)
-#define FLASH_END_ADDR       (uint32_t)(0x081FFFFF)
+#define FLASH_BASE_ADDR      					(uint32_t)(FLASH_BASE)
+#define FLASH_END_ADDR       					(uint32_t)(0x081FFFFF)
 
 /* Base address of the Flash sectors Bank 1 */
-#define ADDR_FLASH_SECTOR_0_BANK1     ((uint32_t)0x08000000) /* Base @ of Sector 0, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_1_BANK1     ((uint32_t)0x08020000) /* Base @ of Sector 1, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_2_BANK1     ((uint32_t)0x08040000) /* Base @ of Sector 2, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_3_BANK1     ((uint32_t)0x08060000) /* Base @ of Sector 3, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_4_BANK1     ((uint32_t)0x08080000) /* Base @ of Sector 4, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_5_BANK1     ((uint32_t)0x080A0000) /* Base @ of Sector 5, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_6_BANK1     ((uint32_t)0x080C0000) /* Base @ of Sector 6, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_7_BANK1     ((uint32_t)0x080E0000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_0_BANK1    			((uint32_t)0x08000000) /* Base @ of Sector 0, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_1_BANK1     			((uint32_t)0x08020000) /* Base @ of Sector 1, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_2_BANK1     			((uint32_t)0x08040000) /* Base @ of Sector 2, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_3_BANK1     			((uint32_t)0x08060000) /* Base @ of Sector 3, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_4_BANK1     			((uint32_t)0x08080000) /* Base @ of Sector 4, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_5_BANK1     			((uint32_t)0x080A0000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_6_BANK1     			((uint32_t)0x080C0000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_7_BANK1     			((uint32_t)0x080E0000) /* Base @ of Sector 7, 128 Kbytes */
 
 /* Base address of the Flash sectors Bank 2 */
-#define ADDR_FLASH_SECTOR_0_BANK2     ((uint32_t)0x08100000) /* Base @ of Sector 8, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_1_BANK2     ((uint32_t)0x08120000) /* Base @ of Sector 9, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_2_BANK2     ((uint32_t)0x08140000) /* Base @ of Sector 10, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_3_BANK2     ((uint32_t)0x08160000) /* Base @ of Sector 11, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_4_BANK2     ((uint32_t)0x08180000) /* Base @ of Sector 12, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_5_BANK2     ((uint32_t)0x081A0000) /* Base @ of Sector 13, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_6_BANK2     ((uint32_t)0x081C0000) /* Base @ of Sector 14, 128 Kbytes */
-#define ADDR_FLASH_SECTOR_7_BANK2     ((uint32_t)0x081E0000) /* Base @ of Sector 15, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_0_BANK2     			((uint32_t)0x08100000) /* Base @ of Sector 0, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_1_BANK2     			((uint32_t)0x08120000) /* Base @ of Sector 1, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_2_BANK2     			((uint32_t)0x08140000) /* Base @ of Sector 2, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_3_BANK2     			((uint32_t)0x08160000) /* Base @ of Sector 3, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_4_BANK2     			((uint32_t)0x08180000) /* Base @ of Sector 4, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_5_BANK2     			((uint32_t)0x081A0000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_6_BANK2     			((uint32_t)0x081C0000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_7_BANK2     			((uint32_t)0x081E0000) /* Base @ of Sector 7, 128 Kbytes */
 
 /**************************************************************************************/
 /********************              debug definitions               ********************/
@@ -56,35 +56,38 @@
 /******************              Ethernet definitions               *******************/
 /**************************************************************************************/
 #define SEND_CIS_LINE_FREQUENCY				    (3000)
+#define IMAGE_HEADER							(1397969715) //01010011 01010011 01010011 00110011 SSS3
+#define IMAGE_HEADER_SIZE						(1)//uint32
+
 
 /* UDP local connection port */
-#define UDP_SERVER_PORT    ((uint16_t)60U)
+#define UDP_SERVER_PORT    						((uint16_t)60U)
 /* UDP remote connection port */
-#define UDP_CLIENT_PORT    ((uint16_t)60U)
+#define UDP_CLIENT_PORT   						((uint16_t)60U)
 
 /*Static DEST IP ADDRESS: DEST_IP_ADDR0.DEST_IP_ADDR1.DEST_IP_ADDR2.DEST_IP_ADDR3 */
-#define DEST_IP_ADDR0   ((uint8_t)192U)
-#define DEST_IP_ADDR1   ((uint8_t)168U)
-#define DEST_IP_ADDR2   ((uint8_t)0U)
-#define DEST_IP_ADDR3   ((uint8_t)1U)
+#define DEST_IP_ADDR0   						((uint8_t)192U)
+#define DEST_IP_ADDR1   						((uint8_t)168U)
+#define DEST_IP_ADDR2   						((uint8_t)0U)
+#define DEST_IP_ADDR3   						((uint8_t)1U)
 
 /*Static IP ADDRESS: IP_ADDR0.IP_ADDR1.IP_ADDR2.IP_ADDR3 */
-#define IP_ADDR0   ((uint8_t) 192U)
-#define IP_ADDR1   ((uint8_t) 168U)
-#define IP_ADDR2   ((uint8_t) 0U)
-#define IP_ADDR3   ((uint8_t) 10U)
+#define IP_ADDR0   								((uint8_t) 192U)
+#define IP_ADDR1   								((uint8_t) 168U)
+#define IP_ADDR2   								((uint8_t) 0U)
+#define IP_ADDR3   								((uint8_t) 10U)
 
 /*NETMASK*/
-#define NETMASK_ADDR0   ((uint8_t) 255U)
-#define NETMASK_ADDR1   ((uint8_t) 255U)
-#define NETMASK_ADDR2   ((uint8_t) 255U)
-#define NETMASK_ADDR3   ((uint8_t) 0U)
+#define NETMASK_ADDR0   						((uint8_t) 255U)
+#define NETMASK_ADDR1   						((uint8_t) 255U)
+#define NETMASK_ADDR2   						((uint8_t) 255U)
+#define NETMASK_ADDR3   						((uint8_t) 0U)
 
 /*Gateway Address*/
-#define GW_ADDR0   ((uint8_t) 192U)
-#define GW_ADDR1   ((uint8_t) 168U)
-#define GW_ADDR2   ((uint8_t) 0U)
-#define GW_ADDR3   ((uint8_t) 1U)
+#define GW_ADDR0   								((uint8_t) 192U)
+#define GW_ADDR1   								((uint8_t) 168U)
+#define GW_ADDR2   								((uint8_t) 0U)
+#define GW_ADDR3   								((uint8_t) 1U)
 
 /**************************************************************************************/
 /********************              DAC definitions                 ********************/
